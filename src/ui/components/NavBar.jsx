0863724c896522ba52c4052284b9c15a68c847e6
Santiago <scastro@cobroinmediato.tech>
2024-09-12 12:13:09 -0300

    features styles

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,59 +1,73 @@
-import { useContext } from 'react';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../auth/context/AuthContext';
+import { useContext } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogout = () => {
     logout();
-    navigate('/', {
+    navigate("/", {
       replace: true,
     });
   };
   return (
-    <nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
-      <Link className='navbar-brand' to='/'>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+      <Link className="navbar-brand" to="/">
         Asociaciones
       </Link>
 
-      <div className='navbar-collapse'>
-        <div className='navbar-nav'>
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
           <NavLink
             className={({ isActive }) =>
-              isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+              isActive ? "nav-item nav-link active" : "nav-item nav-link"
             }
-            to='/marvel'
+            to="/marvel"
           >
             Marvel
           </NavLink>
 
           <NavLink
             className={({ isActive }) =>
-              isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+              isActive ? "nav-item nav-link active" : "nav-item nav-link"
             }
-            to='/dc'
+            to="/dc"
           >
             DC
           </NavLink>
           <NavLink
             className={({ isActive }) =>
-              isActive ? 'nav-item nav-link active' : 'nav-item nav-link'
+              isActive ? "nav-item nav-link active" : "nav-item nav-link"
             }
-            to='/search'
+            to="/search"
           >
             Buscar
           </NavLink>
         </div>
       </div>
 
-      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
-        <ul className='navbar-nav ml-auto'>
-          <span className='nav-item nav-link text-primary'> {user?.name.toUpperCase()}</span>
-          <button className='nav-item nav-link btn' onClick={onLogout}>
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link">
+            {" "}
+            Bienvenid@: <b>{user?.name.toUpperCase()}</b>{" "}
+          </span>
+          <button
+            style={{
+              backgroundColor: "red",
+              paddingLeft: "25px",
+              paddingRight: "25px",
+              borderRadius: "5px",
+              color: "black",
+              fontWeight: "bold",
+              textShadow: "2px 2px 2px white",
+            }}
+            className="nav-item nav-link "
+            onClick={onLogout}
+          >
             Salir
           </button>
-
         </ul>
       </div>
     </nav>
